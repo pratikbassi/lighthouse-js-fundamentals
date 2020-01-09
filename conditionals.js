3950f74f6d57d56e1c56a7313484682e37797b4a
Pratik Bassi <pratik.bassi@mail.utoronto.ca>
2020-01-09 01:15:58 +0000

    More condtitional examples

--- conditionals.js
+++ conditionals.js
@@ -16,3 +16,15 @@ else {
 }
 
 console.log("Now you're ready to go outside!")
+
+
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18){
+  console.log("You are eligible to vote")
+}
+
+if (!raining){
+  console.log("Leave your umbrella at home!")
+}
