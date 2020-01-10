fcf261085ed1a55dcb514f87767da06962d78cd2
Pratik Bassi <pratik.bassi@mail.utoronto.ca>
2020-01-10 19:02:52 +0000

    this contains a loop for an assignment

---
+++ loopy.js
@@ -0,0 +1,17 @@
+let x = 100;
+
+while(x < 201){
+  if (x % 3 === 0 && x % 4 !== 0){
+    console.log("Loopy")
+  }
+  else if( x % 4 === 0 && x % 3 !== 0){
+    console.log('Lighthouse')
+  }
+  else if(x % 3 === 0 && x % 4 === 0){
+    console.log('LoopyLighthouse')
+  }
+  else{
+    console.log(x)
+  }
+  x++;
+}
